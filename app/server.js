3c7d005f35d1d3f48da015428d111ffce5e7c208
Joachim <joachim.vanpraet@flexso.com>
2019-11-12 11:39:16 +0100

    initial commit

---
+++ app/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const handleRequest = (req, res) => {
+
+  const request = {
+	  method: req.method,
+	  baseUrl: req.baseUrl,
+	  uri: req.url,
+	  host: req.hostname,
+	  headers: {
+		  ... req.headers
+	  },
+	  body: req.body
+  };
+  
+  console.log( request );
+  
+  
+  res.send(request);
+}
+
+app.all('*', handleRequest);
+
+const port = process.env.PORT || 3000;;
+app.listen(port, function () {
+  console.log('myapp listening on port ' + port);
+});
